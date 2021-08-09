775a1f632c1397c86f1f363440e36e071a3bfef4
alperkan1 <seanhack@gmail.com>
2021-08-09 16:13:59 +0000

    -a run game working

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,18 +7,26 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+    runGame("addition");
 });
 /**
  * the main game loop to crate random numbers
  * and resets 
  */
-function runGame() {
+function runGame(gameType) {
+    //creates a number between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1,num2)
+    } else alert (`Unknown game type: ${gameType}`)
+    throw `Unknow game type: ${gameType}. Aborting!`;
+
 }
 
 
@@ -43,8 +51,11 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
 
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 
 }
 
