0958aef28a088f9d5e17ef727852f705d2876734
alperkan1 <seanhack@gmail.com>
2021-08-09 15:14:21 +0000

    -aadd table of content for js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,17 @@
-document.addEventListener("DOMcontentLoaded", function () {
-            let buttons = document.getElementsByTagName("button");
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
 
-            for (let button of buttons) {
-                button.addEventListener("click", function () {
-                            if (this.getAttribute("data-type") === "submit") {
-                                    alert("You clicked Submit!");
-                                } else {
-                                    let gameType = this.getAttribute("data-type")
-                                    alert(`You Clicked ${gameType}`);
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
             }
-        })
+        });
     }
-})
+});
 
 function runGame() {
 
