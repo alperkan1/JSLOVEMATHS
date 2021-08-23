2951d4ea4dc1408d170931db5f2d67bf1eb706b2
alperkan1 <seanhack@gmail.com>
2021-08-23 11:57:32 +0000

    add subtract

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,12 @@ document.addEventListener("DOMContentLoaded", function () {
 		});
 	}
 
+	document.getElementById("answer-box").addEventListener("keydown", function (event) {
+		if (event.key === "Enter") {
+			checkAnswer();
+		}
+	})
+
 	runGame("addition");
 
 });
@@ -25,8 +31,9 @@ document.addEventListener("DOMContentLoaded", function () {
  */
 function runGame(gameType) {
 
-	document.getElementById("answer-box").value="";
+	document.getElementById("answer-box").value = "";
 	document.getElementById("answer-bax").focus();
+
 
 	// Creates two random numbers between 1 and 25
 	let num1 = Math.floor(Math.random() * 25) + 1;
@@ -122,6 +129,9 @@ function displayMultiplyQuestion(operand1, operand2) {
 	document.getElementById('operator').textContent = "x";
 }
 
-function displayDivideQuestion() {
+function displayDivideQuestion("operand1, operand2") {
+	{	document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+		document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+		document.getElementById('operator').textContent = "/";
 
 }
