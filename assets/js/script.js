1a9224613ac8f18897d24443fdf4579051e92f55
alperkan1 <seanhack@gmail.com>
2021-08-23 11:58:52 +0000

    add divide game

--- assets/js/script.js
+++ assets/js/script.js
@@ -129,9 +129,8 @@ function displayMultiplyQuestion(operand1, operand2) {
 	document.getElementById('operator').textContent = "x";
 }
 
-function displayDivideQuestion("operand1, operand2") {
-	{	document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
-		document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
-		document.getElementById('operator').textContent = "/";
-
+function displayDivideQuestion(operand1, operand2) {
+	document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+	document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+	document.getElementById('operator').textContent = "/";
 }
